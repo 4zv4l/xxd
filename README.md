9dc7c298555ae4864c54794852b0cd46900eac3f
Simon Blacks <simon.blacks@barecovetech.com>
2024-07-29 18:42:07 +0800

    init: update readme and push base code

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # xxd
+
 basic xxd clone in Zig
+
+## usage
+
+```
+usage: xxd [option] [file]
+
+Options:
+    -r     reverse operation (hex dump -> bytes)
+```
+
+dump file
+
+```
+$ ./xxd test
+00000000: 4865 6c6c 6f2c 2057 6f72 6c64 2021 2046  Hello, World ! F
+00000010: 6f6f 2042 6172 0a                        oo Bar.
+```
+
+load from dump (hex to bytes)
+
+```
+$ ./xxd -r test_hex
+Hello, World ! Foo Bar
+```
+
+it can also read from stdin if piped
+
+```
+$ ./xxd test | ./xxd -r | ./xxd
+00000000: 4865 6c6c 6f2c 2057 6f72 6c64 2021 2046  Hello, World ! F
+00000010: 6f6f 2042 6172 0a                        oo Bar.
+```
+
+## how to compile
+
+with the zig compiler
+
+```
+$ zig build-exe -D ReleaseSmall xxd.zig
+```
---
+++ xxd.zig
@@ -0,0 +1,115 @@
+const std = @import("std");
+const io = std.io;
+const allocator = std.heap.page_allocator;
+const eprint = std.debug.print;
+
+pub fn main() u8 {
+    const args = std.process.argsAlloc(allocator) catch |err| {
+        eprint("xxd: argsAlloc(): {s}\n", .{@errorName(err)});
+        return 1;
+    };
+    defer std.process.argsFree(allocator, args);
+
+    // parse arguments
+    var reverse: bool = false;
+    const file = blk: {
+        if (args.len > 1 and std.mem.eql(u8, args[1], "-r")) reverse = true;
+
+        if (!io.getStdIn().isTty()) break :blk io.getStdIn();
+
+        const filename = if (args.len == 2) args[1] else if (args.len == 3) args[2] else {
+            eprint("usage: xxd [option] [file]\n\n", .{});
+            eprint("Options:\n    -r     reverse operation (hex dump -> bytes)\n", .{});
+            return 2;
+        };
+
+        break :blk std.fs.cwd().openFile(filename, .{}) catch |err| {
+            eprint("xxd: {s}: {s}\n", .{ filename, @errorName(err) });
+            return 3;
+        };
+    };
+    defer file.close();
+
+    // setup buffered IO
+    var bout = io.bufferedWriter(io.getStdOut().writer());
+    var bin = io.bufferedReader(file.reader());
+
+    if (reverse) {
+        load(bin.reader(), bout.writer()) catch |err| {
+            eprint("xxd: load(): {s}\n", .{@errorName(err)});
+            return 4;
+        };
+    } else {
+        dump(bin.reader(), bout.writer()) catch |err| {
+            eprint("xxd: dump(): {s}\n", .{@errorName(err)});
+            return 5;
+        };
+    }
+
+    // flush buffered writer (stdout)
+    bout.flush() catch return 6;
+    return 0;
+}
+
+// load reader to writer from hex to byte
+// reverse of dump()
+fn load(reader: anytype, writer: anytype) !void {
+    var buff: [68]u8 = undefined; // counting newline
+    while (true) {
+        const line = try reader.readUntilDelimiterOrEof(&buff, '\n') orelse break;
+        const start_idx = (std.mem.indexOf(u8, line, ": ") orelse return error.WrongFormat) + 2;
+        const end_idx = std.mem.indexOf(u8, line, "  ") orelse return error.WrongFormat;
+        var hexit = std.mem.splitScalar(u8, line[start_idx..end_idx], ' ');
+        while (hexit.next()) |h| {
+            var byteit = std.mem.window(u8, h, 2, 2);
+            while (byteit.next()) |b| try writer.writeByte(try std.fmt.parseUnsigned(u8, b, 16));
+        }
+    }
+}
+
+// dump reader to writer as hex and ascii with an offset
+// ex: abcdefghijklmno\n => 00000000: 6162 6364 6566 6768 696a 6b6c 6d6e 6f0a  abcdefghijklmno.
+fn dump(reader: anytype, writer: anytype) !void {
+    var chunk: [16]u8 = undefined;
+    var hexview: [40]u8 = undefined;
+    var asciiview: [16]u8 = undefined;
+    var offset: usize = 0;
+
+    while (true) : (offset += 0x10) {
+        const len = try reader.read(&chunk);
+        if (len == 0) break;
+
+        try writer.print("{x:0>8}: {s: <40} {s}\n", .{
+            offset,
+            asHex(chunk[0..len], &hexview),
+            asAscii(chunk[0..len], &asciiview),
+        });
+    }
+}
+
+// write chunk to hex per pair of two bytes as hex, separated with a space
+// ex: \n\naa => 0a0a 6161
+fn asHex(chunk: []const u8, hex: []u8) []const u8 {
+    var need_space: bool = false;
+    var idx: usize = 0;
+    for (chunk) |c| {
+        if (need_space) {
+            const t = std.fmt.bufPrint(hex[idx..], "{x:0>2} ", .{c}) catch unreachable;
+            idx += t.len;
+        } else {
+            const t = std.fmt.bufPrint(hex[idx..], "{x:0>2}", .{c}) catch unreachable;
+            idx += t.len;
+        }
+        need_space = !need_space;
+    }
+    return hex[0..idx];
+}
+
+// write chunk to ascii replacing non printable char by a dot
+// ex: a[\n => a[.
+fn asAscii(chunk: []const u8, ascii: []u8) []const u8 {
+    for (chunk, ascii[0..chunk.len]) |c, *a| {
+        a.* = if (std.ascii.isPrint(c)) c else '.';
+    }
+    return ascii[0..chunk.len];
+}
